102821d31be6725a085b63fef5b414fa099d8f8b
Gord West <52170149+Westabago@users.noreply.github.com>
2020-12-03 14:50:53 -0700

    including heat map

--- charts/lineChart.js
+++ charts/lineChart.js
@@ -6,27 +6,32 @@ function makeLineChart() {
         .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
         .append("g")
         .attr("transform", "translate(150,-100)");
-        
-    // var legend = d3.select('#LINE_LEGEND')
-    //     .append("svg")
-    //     .attr("width", 300)
-    //     .attr("height", HEIGHT)
-    //     .attr("transform", "translate(-200,-100)");
+
+    var heatMap = d3.select("#HEAT_MAP")
+        .append("svg")
+        .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+        .attr("height", HEIGHT)
+        .append("g")
+        .attr("transform",
+            "translate(" + MARGIN.LEFT + "," + MARGIN.TOP + ")");
+
 
     //Read the data
-    d3.csv("data/FormattedData1.csv")
+    d3.csv("data/FormattedData.csv")
         .then(function(d) { 
         d.forEach(function(d) {
             d.Date = parseDate(d.Date);
             d.Price = parseFloat(d.Price);
         });
 
-        console.log(d)
-
         // initialize some variables for later
         var minDate = d3.min(d, function(d) {return d.Date;});
         var maxDate = d3.max(d, function(d) {return d.Date;});
         var maxPrice = d3.max(d, function(d) {return d.Price;});
+
+        // Labels of row and columns using the unique names: 'date' and 'ticker'
+        var myGroups = groupHeader(d, "date")
+        var myVars = groupHeader(d, "ticker")
 
         // Add X axis
         var x = d3.scaleTime()
@@ -54,6 +59,119 @@ function makeLineChart() {
             .attr("font-size","16")
             .attr("fill","black")
 
+
+        // Build X scales and axis for heatMap
+        var x_heatMap = d3.scaleBand()
+            .range([0, WIDTH - MARGIN.RIGHT])
+            .domain(myGroups)
+            .padding(0.05);
+        heatMap.append("g")
+            .style("font-size", 14)
+            .attr("transform", "translate(0," + (HEIGHT - 200) + ")")
+            .call(d3.axisBottom(x_heatMap).tickSize(0))
+            .select(".domain").remove()
+
+        // Build Y scales and axis:
+        var y_heatMap = d3.scaleBand()
+            .range([HEIGHT - MARGIN.BOTTOM - MARGIN.TOP, 0 ])
+            .domain(myVars)
+            .padding(0.05);
+        heatMap.append("g")
+            .style("font-size", 14)
+            .call(d3.axisLeft(y_heatMap).tickSize(0))
+            .select(".domain").remove()
+
+        // Build color scale
+        var myColor = d3.scaleSequential()
+            .interpolator(d3.interpolateInferno)
+            .domain([1,100])
+
+        // create a tooltip
+        var tooltip_heatMap = d3.select("#HEAT_TOOLTIP")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "5px")
+            .style("padding", "5px")
+
+        // Three function that change the tooltip when user hovers
+        var mouseover = function(d) {
+            tooltip_heatMap
+                .style("opacity", 1)
+            d3.select(this)
+                .style("stroke", "black")
+                .style("opacity", 1)
+        }
+        var mousemove = function(d) {
+            tooltip_heatMap
+                .html("The price of " + d.ticker + " on "  + d.date + " was " + d.value)
+                .style("left", (d3.mouse(this)[0]+70) + "px")
+                .style("top", (d3.mouse(this)[1]) + "px")
+        }
+        var mouseleave = function(d) {
+            tooltip_heatMap
+                .style("opacity", 0)
+            d3.select(this)
+                .style("stroke", "none")
+                .style("opacity", 0.8)
+        }
+
+        // add the squares for the map
+        heatMap.selectAll()
+            .data(d, function(d) {return d.date+':'+d.ticker;})
+            .enter()
+            .append("rect")
+            .attr("x", function(d) { return x(d.date) })
+            .attr("y", function(d) { return y(d.ticker) })
+            .attr("rx", 4)
+            .attr("ry", 4)
+            .attr("width", x_heatMap.bandwidth() )
+            .attr("height", y_heatMap.bandwidth() )
+            .style("fill", function(d) { return myColor(d.value)} )
+            .style("stroke-width", 4)
+            .style("stroke", "none")
+            .style("opacity", 0.8)
+            .on("mouseover", function(event,d){
+                tooltip
+                    .html("The price of " + d.ticker + " on "  + d.date + " was " + d.value)
+                    .style("opacity", 1)
+                    .style("left", (event.screenX+70) + "px")
+                    .style("top", (event.screenY) + "px")
+                d3.select(this)
+                    .style("stroke", "black")
+                    .style("opacity", 1)
+            })
+            .on("mouseleave", function(event,d){
+                tooltip
+                    .style("opacity", 0)
+                d3.select(this)
+                    .style("stroke", "none")
+                    .style("opacity", 0.8)
+            })
+    
+
+        // Add title
+        heatMap.append("text")
+            .attr("x", 0)
+            .attr("y", -50)
+            .attr("text-anchor", "left")
+            .style("font-size", "22px")
+            .text("An ETF Heatmap");
+
+    // Add subtitle
+        heatMap.append("text")
+            .attr("x", 0)
+            .attr("y", -20)
+            .attr("text-anchor", "left")
+            .style("font-size", "14px")
+            .style("fill", "grey")
+            .style("max-width", 400)
+            .text("This heatmap shows the value on the last trading day for each year of our data ");
+// #################################################################################################################
+
         // add a clipPath
         var clip = svg.append("defs").append("svg:clipPath")
             .attr("id", "clip")
@@ -65,12 +183,32 @@ function makeLineChart() {
 
         // add brushing
         var brush = d3.brushX()                  
-            .extent( [ [0,200], [WIDTH, HEIGHT] ] )  
+            .extent( [ [0,200], [WIDTH, HEIGHT]])  
             .on("end", updateChart)             
 
         var line = svg.append('g')
             // .attr("transform", "translate(0," + (MARGIN.BOTTOM) + ")")
             .attr("clip-path", "url(#clip)")
+
+        const tooltip = d3.select('#TOOL_TIP');
+        const tooltipLine = line.append('line')
+                                .attr("class", "tooltip")
+                                .attr('stroke', 'black')
+                .attr('x1', 300)
+                .attr('x2', 300)
+                .attr('y1', 0)
+                .attr('y2', HEIGHT);
+
+        // tipBox = line.append('rect')
+        //     .attr('width', WIDTH)//-MARGIN.RIGHT)
+        //     .attr('height', HEIGHT)//-MARGIN.TOP-MARGIN.BOTTOM)
+        //     .attr('opacity', 0)
+        //     //.attr("transform", "translate(0," + 200 + ")")
+        //     .on('mousemove', function(d) {
+        //         console.log("mouse in ")
+        //     })
+        //     // drawTooltip)
+        //     .on('mouseout', removeTooltip);
 
         // function to draw a line on the chart
         var drawLine = d3.line()
@@ -101,9 +239,16 @@ function makeLineChart() {
             .attr("transform", "translate(0," + (MARGIN.BOTTOM) + ")")
 
         // add the brushing
-        line
-            .append("g")
+        tipBox = line.append("g")
             .attr("class", "brush")
+            .on('mousemove', function() {
+                console.log("mouse move")
+                drawTooltip
+            })
+            .on('mouseout',function() {
+                console.log("mouse out") 
+                removeTooltip
+            })
             .call(brush);
 
         // add a dot for each ETF
@@ -142,6 +287,31 @@ function makeLineChart() {
             .style("alignment-baseline", "middle")
             .style("font-size", "20")
 
+        function removeTooltip() {
+            if (tooltip) tooltip.style('display', 'none');
+            if (tooltipLine) tooltipLine.attr('stroke', 'none');
+            }
+            
+        function drawTooltip() { 
+                  
+            tooltipLine
+                .attr('stroke', 'black')
+                .attr('x1', 300)
+                .attr('x2', 300)
+                .attr('y1', 0)
+                .attr('y2', HEIGHT);
+            
+            tooltip.html()
+                .style('display', 'block')
+                .style('left', d3.event.pageX + 20)
+                .style('top', d3.event.pageY - 20)
+                .selectAll()
+                .data(states).enter()
+                .append('div')
+                .style('color', d => d.color)
+                .html(d => d.name + ': ' + d.history.find(h => h.year == year).population);
+        }
+
         // remove/add line to chart     
         function toggleLine(name, state) {
             if (state) {
@@ -160,43 +330,68 @@ function makeLineChart() {
 
         // update the chart for given boundaries
         function updateChart(event) {
-        extent = event.selection
-        maxPrice = d3.max(d, function(d) {return d.Price;});
+            extent = event.selection
+            // console.log(y.invert(extent[0]), y.invert(extent[1]))
+            // console.log(y.invert(extent[0]), y.invert(extent[1]))
+            
+            // if no selection, back to initial coordinate. Otherwise, update X axis domain
+            if(!extent){
+                if (!idleTimeout) return idleTimeout = setTimeout(idled, 350);
+                x.domain([ 4,8])
+            }else{
+                x.domain([x.invert(extent[0]), x.invert(extent[1])])
+                // y.domain([0, y.invert(extent[1])])
+                line.select(".brush").call(brush.move, null) // This remove the grey brush area when selection is made
+            }
+
+            // update x axis 
+            xAxis
+                .transition()
+                .duration(700)
+                .call(d3.axisBottom(x));
+            
+            // // update x axis 
+            // yAxis
+            //     .transition()
+            //     .duration(700)
+            //     .call(d3.axisLeft(y));
+
+            // update line position
+            line
+                .selectAll('.line')
+                .transition()
+                .duration(700)
+                .attr('d', function(d) { 
+                    return drawLine(d[1])
+                })
+        }
+
+        // function to clear line chart - activated by button
+        function removeAllLines() {
+            console.log("remove all");
+        }
+
+        // function to make all lines visible - activated by button
+        function addAllLines() {
+            console.log("add back");
+        }
         
-        // if no selection, back to initial coordinate. Otherwise, update X axis domain
-        if(!extent){
-            if (!idleTimeout) return idleTimeout = setTimeout(idled, 350);
-            x.domain([ 4,8])
-        }else{
-            x.domain([x.invert(extent[0]), x.invert(extent[1])])
-            line.select(".brush").call(brush.move, null) // This remove the grey brush area when selection is made
-        }
-
-        // update axis 
-        xAxis
-            .transition()
-            .duration(1000)
-            .call(d3.axisBottom(x));
-
-        // update line position
-        line
-            .selectAll('.line')
-            .transition()
-            .duration(1000)
-            .attr('d', function(d) { 
-                return drawLine(d[1])
-            })
-        }
-
         // reinitialize chart on double click
         svg.on("dblclick",function(){
             x.domain([minDate, maxDate])
+            // y.domain([0, maxPrice])
             xAxis
                 .transition()
                 .call(d3.axisBottom(x));
             xAxis.selectAll(".tick text")
                 .attr("font-size","16")
                 .attr("fill","black");
+            // yAxis
+            //     .transition()
+            //     .call(d3.axisLeft(y));
+            // yAxis.selectAll(".tick text")
+            //     .attr("font-size","16")
+            //     .attr("fill","black");
             line
                 .selectAll('.line')
                 .transition()
